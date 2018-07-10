554c3f731e175da7f313328b66dc55fc62edc1a3
simbrandstrup <40585356+simbrandstrup@users.noreply.github.com>
2018-07-10 18:21:55 -0400

    changed casesensitive char

--- src/test/testCheckEmailForEdu.js
+++ src/test/testCheckEmailForEdu.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var checkEmailforEdu = require('../utilities/checkEmailForEdu');
+
 describe('checkEmailforEdu', function () {
     it("confirms that email contains 'edu'", function () {
         assert.equal(true, checkEmailforEdu('mo@harvard.edu'));
